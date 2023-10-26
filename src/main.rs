6d704993b4b65daf7bd0c7f17c8d620c228f253b
akashTwid <akash.dubey@twidpay.com>
2023-10-27 01:36:09 +0530

    basic setup started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind("localhost:8080")?
+        .run()
+        .await
+}
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
